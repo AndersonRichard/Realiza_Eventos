30a509b3029081ebe9209db0d9fc6ee17ed83c70
AndersonRichard <richard-r3@hotmail.com>
2023-05-28 15:38:21 -0300

    Criacao da classe endereco

---
+++ Realiza_Eventos/src/models/Endereco.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Endereco {
+    private String id;
+    private String pais;
+    private String estado;
+    private String cep;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String complemento;
+
+}
