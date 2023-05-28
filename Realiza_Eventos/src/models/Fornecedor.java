4e138fdb1382065c198770adaf9a3e774427a014
Luiz Felipe <lfpereira323@gmail.com>
2023-05-28 15:47:27 -0300

    commit teste

---
+++ Realiza_Eventos/src/models/Fornecedor.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Fornecedor {
+
+    public static void imprimir() {
+        System.out.println("fornecedor");
+    }
+}
--- Realiza_Eventos/src/models/Principal.java
+++ Realiza_Eventos/src/models/Principal.java
@@ -3,5 +3,6 @@ package models;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        Fornecedor.imprimir();
     }
 }
