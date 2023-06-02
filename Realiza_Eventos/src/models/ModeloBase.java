4d9c97bda597d43071de2688ebf83aa38551e9ad
jeanarthur <jean.arthursa@gmail.com>
2023-06-02 20:21:43 -0300

    Adiciona model ModeloBase

---
+++ Realiza_Eventos/src/models/ModeloBase.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.UUID;
+
+public class ModeloBase {
+
+    protected String id;
+
+    public ModeloBase(){
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public ModeloBase clone(){
+        ModeloBase clone = new ModeloBase();
+        clone.id = this.id;
+        return clone;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        //Verifica se os objetos comparados são da mesma classe E se possuem o mesmo id
+        return o.getClass().equals(this.getClass()) && this.id.equals(((ModeloBase) o).getId());
+    }
+
+    @Override
+    public String toString(){
+        return "Id: " + this.id;
+    }
+
+}
