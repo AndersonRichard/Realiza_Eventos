bdb20671fd3cc5904cc30bcb5a56d38d9ee974bb
jeanarthur <jean.arthursa@gmail.com>
2023-06-03 18:20:21 -0300

    Atualiza views/Cliente - adiciona metodos

--- Realiza_Eventos/src/views/ClienteView.java
+++ Realiza_Eventos/src/views/ClienteView.java
@@ -1,5 +1,9 @@
 package views;
 
+import models.Cliente;
+import services.ClienteService;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClienteView {
@@ -10,13 +14,13 @@ public class ClienteView {
         while (true) {
             System.out.println("Escolha uma opção:");
             System.out.println("1. Consultar");
-            System.out.println("0. Sair");
+            System.out.println("0. Voltar");
             System.out.print("Opção: ");
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-
+                    consultar();
                     break;
                 case 0:
                     return;
@@ -25,4 +29,44 @@ public class ClienteView {
             }
         }
     }
+
+    public void consultar(){
+        Scanner scanner = new Scanner(System.in);
+        ClienteService clienteService = new ClienteService();
+
+        while (true) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Pesquisar por Id");
+            System.out.println("2. Listar todos");
+            System.out.println("0. Voltar");
+            System.out.print("Opção: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o Id: ");
+                    Cliente clienteEncontrado = clienteService.read(scanner.nextLine());
+                    if (clienteEncontrado == null) {
+                        System.out.println("Cliente não localizado!");
+                        break;
+                    }
+                    System.out.println(" -- CLIENTE --");
+                    System.out.println(clienteEncontrado);
+                    break;
+                case 2:
+                    ArrayList<Cliente> clientes = clienteService.read();
+                    if (clientes.size() > 0){ System.out.println(" -- CLIENTES --"); }
+                    for (Cliente cliente : clientes){
+                        System.out.println(cliente);
+                        System.out.println("-----");
+                    }
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+
 }
