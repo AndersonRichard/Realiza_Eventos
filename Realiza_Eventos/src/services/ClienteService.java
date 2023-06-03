8737d939f19da7f42268588fb2f37d387bcbc4eb
jeanarthur <jean.arthursa@gmail.com>
2023-06-03 19:18:52 -0300

    Corrige todos os services e remove ServiceBase

--- Realiza_Eventos/src/services/ClienteService.java
+++ Realiza_Eventos/src/services/ClienteService.java
@@ -4,10 +4,49 @@ import models.Cliente;
 
 import java.util.ArrayList;
 
-public class ClienteService extends ServiceBase<Cliente> {
+public class ClienteService {
     public static ArrayList<Cliente> dados;
 
     public ClienteService() {
-        dados = super.dados;
+        if (dados == null){
+            dados = new ArrayList<>();
+        }
+    }
+
+    // Cria o registro na base de dados
+    public void create(Cliente registro){
+        dados.add(registro);
+    }
+
+    // Retorna todos os registros
+    public ArrayList<Cliente> read(){
+        return dados;
+    }
+
+    // Retorna o registro com o id especificado
+    public Cliente read(String id){
+        for (Cliente registro : new ArrayList<>(dados)){
+            if (registro.getId().equals(id)){
+                return registro.clone();
+            }
+        }
+        return null;
+    }
+
+    // Atualiza o registro
+    public void update(Cliente registro){
+        if (new ArrayList<>(dados).contains(registro)){
+            dados.remove(registro);
+            dados.add(registro);
+        }
+    }
+
+    // Deleta o registro conforme o id informado
+    public void delete(String id){
+        for (Cliente registro : new ArrayList<>(dados)){
+            if (registro.getId().equals(id)){
+                dados.remove(registro);
+            }
+        }
     }
 }
--- Realiza_Eventos/src/services/OpcaoService.java
+++ Realiza_Eventos/src/services/OpcaoService.java
@@ -1,15 +1,55 @@
 package services;
 
 import models.Opcao;
+import models.Opcao;
 
 import java.util.ArrayList;
 
-public class OpcaoService extends ServiceBase<Opcao> {
+public class OpcaoService {
 
     public static ArrayList<Opcao> dados;
 
-    public OpcaoService(){
-        dados = super.dados;
+    public OpcaoService() {
+        if (dados == null){
+            dados = new ArrayList<>();
+        }
+    }
+
+    // Cria o registro na base de dados
+    public void create(Opcao registro){
+        dados.add(registro);
+    }
+
+    // Retorna todos os registros
+    public ArrayList<Opcao> read(){
+        return dados;
+    }
+
+    // Retorna o registro com o id especificado
+    public Opcao read(String id){
+        for (Opcao registro : new ArrayList<>(dados)){
+            if (registro.getId().equals(id)){
+                return registro.clone();
+            }
+        }
+        return null;
+    }
+
+    // Atualiza o registro
+    public void update(Opcao registro){
+        if (new ArrayList<>(dados).contains(registro)){
+            dados.remove(registro);
+            dados.add(registro);
+        }
+    }
+
+    // Deleta o registro conforme o id informado
+    public void delete(String id){
+        for (Opcao registro : new ArrayList<>(dados)){
+            if (registro.getId().equals(id)){
+                dados.remove(registro);
+            }
+        }
     }
 
 }
