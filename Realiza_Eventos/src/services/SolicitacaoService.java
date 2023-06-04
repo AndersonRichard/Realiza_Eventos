263f8ac8fe74f0c1369cdf64748747c62dc3da75
jeanarthur <jean.arthursa@gmail.com>
2023-06-04 20:36:56 -0300

    Adiciona exportação para Txt

--- Realiza_Eventos/src/services/SolicitacaoService.java
+++ Realiza_Eventos/src/services/SolicitacaoService.java
@@ -1,6 +1,12 @@
 package services;
 
+import models.Opcao;
+import models.Servico;
 import models.Solicitacao;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class SolicitacaoService {
@@ -60,5 +66,69 @@ public class SolicitacaoService {
     private ArrayList<Solicitacao> load(){
         ArmazenamentoService armazenamentoService = new ArmazenamentoService();
         return (ArrayList<Solicitacao>) armazenamentoService.load("solicitacoes");
+    }
+
+    public String exportTxt(Solicitacao solicitacao){
+        String relatorio = String.format(
+                """
+                  ========================================================================================
+                    SOLICITAÇÃO DE ORÇAMENTO
+                Id: %s
+                Data: %s
+                
+                    DADOS DO EVENTO
+                Evento: %s
+                Descrição: %s
+                
+                Serviços:
+                """
+                ,
+                solicitacao.getId(),
+                solicitacao.getDataHoraDaSolicitacaoStr(),
+                solicitacao.getEvento().getNome(),
+                solicitacao.getEvento().getDescricao());
+
+        for (Servico servico : solicitacao.getEvento().getServicos()){
+            relatorio += String.format("\t%s\n\t%s\n", servico.getNome(), servico.getDescricao());
+            for (Opcao opcao : servico.getOpcoes()){
+                relatorio += String.format("\t%s\n\t%s\n", opcao.getNome(), opcao.getDescricao());
+            }
+        }
+
+        relatorio += String.format("""
+                Endereço: %s
+                Inicio: %s
+                Término: %s
+                Observações: %s
+                
+                    INFORMAÇÕES DO CLIENTE
+                Nome: %s
+                Telefone: %s
+                E-mail: %s
+                
+                  ========================================================================================
+                """
+                ,
+                solicitacao.getEnderecoSelecionado().getRua(),
+                solicitacao.getInicioEventoDataHora(),
+                solicitacao.getTerminoEventoDataHora(),
+                solicitacao.getObservacoes(),
+                solicitacao.getCliente().getNome(),
+                solicitacao.getCliente().getTelefone(),
+                solicitacao.getCliente().getEmail()
+        );
+
+        String absPath = new File("").getAbsolutePath() + "\\relatorios\\";
+        try{
+            Files.createDirectories(Path.of(absPath));
+            OutputStreamWriter bufferOut = new OutputStreamWriter(
+                    new FileOutputStream(absPath + "Solicitacao_" + solicitacao.getId() + ".txt"),"UTF-8");
+            bufferOut.write(relatorio);
+            bufferOut.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return absPath + "Solicitacao_" + solicitacao.getId() + ".txt";
     }
 }
